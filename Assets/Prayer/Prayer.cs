b84ab89e2be4228cc66bb047bb5ce5ab6637a65a
福村美瑛 <ktc26a31a0030@edu.kyoto-tech.ac.jp>
2026-06-02 19:50:25 +0900

    6月2日

--- Assets/Prayer/Prayer.cs
+++ Assets/Prayer/Prayer.cs
@@ -1,19 +1,33 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Prayer : MonoBehaviour
 {
-    public float speed = 1f;
-    private Rigidbody2D rb;
-    public float jump = 1;
-    public float jamp_altitude = 0.66f;
-    bool invincible = false;
-    bool dead = false;
-    bool clear = false;
+    Vector2 inputVec;
+    public float speed = 3;
+    Rigidbody2D rb;
+    public float jumpPower = 8f;
+    [Header("jumpPowerと同じ数値")]
+    public float jumpOriginal = 8;
+    public float jump = 0f;
+    bool goJump = false;
+    public float jump_altitude = 0.66f;
+    [Header("ジャンプ回数やjunp_altitudeにより変える")]
+    public float ccc = 1.002f;
+    bool onGround = false;
+    public LayerMask groundLayer;
     bool ingame = true;
+    void OnMove(InputValue Value)
+    {
+        inputVec = Value.Get<Vector2>();
+    }
+    void OnJump(InputValue Value)
+    {
+        goJump = true;
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    { 
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -22,34 +36,65 @@ public class Prayer : MonoBehaviour
     {
         if (ingame == true)
         {
-            float x = Input.GetAxis("Horizontal");
-            rb.linearVelocity = new Vector2(x * speed, 0);
-
-            if (jump == 1)
+            int number = 1;
+            onGround = Physics2D.CircleCast(transform.position,
+                                            0.2f,
+                                            Vector2.down,
+                                            0.15f,
+                                            groundLayer);
+            if (onGround || inputVec.x != 0)
             {
-                invincible = true;
+                rb.linearVelocity = new Vector2(inputVec.x * speed, rb.linearVelocity.y);
             }
-            else if (jump < 1)
+            if (inputVec.x > 0)
             {
-                invincible = false;
+                transform.localScale = new Vector2(1, 1);
             }
-
-        }
-        if (dead == true)
-        {
-            ingame = false;
-        }
-        if (clear  == true)
-        {
-            ingame = false;
-        }
-    }
-    void FixedUpdate()
-    {
-        if (ingame == true)
-        {
+            else if (inputVec.x < 0)
+            {
+                transform.localScale = new Vector2(-1, 1);
+            }
+            if (goJump && jumpPower > ccc)
+            {
+                if (number < 5)
+                {
+                    jump = jumpPower;
+                    Vector2 v = new Vector2(0, jumpPower);
+                    rb.AddForce(v, ForceMode2D.Impulse);
+                }
+                if (number > 4)
+                {
+                    Vector2 v = new Vector2(0, jump);
+                    rb.AddForce(v, ForceMode2D.Impulse);
+                }
+                
+                jumpPower = jumpPower * jump_altitude;
+                Debug.Log("ジャンプ回数は" + number);
+                goJump = false;
+                number = number + 1;
+            }
+            else if (onGround)
+            {
+                jumpPower = jumpOriginal;
+                number = 1;
+            }
             
         }
-        
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Goal")
+        {
+            ingame = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Dead")
+        {
+            ingame = false;
+        }
+    }
+
 }
+
